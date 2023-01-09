7444868feb55fe1e3a3a82063db6f616ee830008
Thi My Linh Bui <thbui@c3r8s4.42prague.com>
2023-01-09 19:08:26 +0100

    shell

---
+++ Shell/shell01/ex07/r_dwssap.sh
@@ -0,0 +1,1 @@
+cat /etc/passwd | sed '/^#/d' | awk "NR%2==0" | awk -F':' '{print$1}' | rev | sort -r | awk 'NR>= ENVIRON["FT_LINE1"] && NR<= ENVIRON["FT_LINE2"]' | tr '\n' ' ' | sed 's+ +, +g' | sed 's+, $+.+g' | tr -d '\n'
